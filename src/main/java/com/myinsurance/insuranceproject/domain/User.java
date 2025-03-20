2d8985f28f6d314a7173c65e41635fbc022fea57
yellowring <honghee0510@gmail.com>
2025-03-20 23:03:18 +0900

    first commit

---
+++ src/main/java/com/myinsurance/insuranceproject/domain/User.java
@@ -0,0 +1,36 @@
+package com.myinsurance.insuranceproject.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String role = "USER";
+
+    private LocalDateTime createAt = LocalDateTime.now();
+
+}
